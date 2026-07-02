fab31f6580e44142405cf611d9f3ebf07a8efbae
Muneeb <muhaamadmuneeb777@gmail.com>
2026-07-02 20:53:48 +0500

    first program

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+  print('Enter the first operand: ');
+  num num1 = num.parse(stdin.readLineSync()!);
+  print('Enter the required operator: ');
+  String operator = stdin.readLineSync()!;
+  print('Enter the second operand: ');
+  num num2 = num.parse(stdin.readLineSync()!);
+  if (operator == "+") {
+    print("$num1 + $num2 =${num1 + num2}");
+  } else if (operator == "-") {
+    print("$num1 - $num2 = ${num1 - num2}");
+  } else if (operator == "/") {
+    print("$num1 / $num2 = ${num1 / num2}");
+  } else if (operator == "*") {
+    print("$num1 * $num2 = ${num1 * num2}");
+  } else {
+    print("The result not found");
+  }
+}
