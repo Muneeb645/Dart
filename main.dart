8c93dd070ccbff30acbe3e1982a49665dad1810e
Muneeb <muhaamadmuneeb777@gmail.com>
2026-07-03 20:33:34 +0500

    code of temperature calculation

--- main.dart
+++ main.dart
@@ -1,22 +1,24 @@
 import 'dart:io';
 
 void main() {
-  print('Enter the first operand: ');
-  num num1 = num.parse(stdin.readLineSync()!);
-  print('Enter the required operator: ');
-  String operator = stdin.readLineSync()!;
-  print('Enter the second operand: ');
-
-  num num2 = num.parse(stdin.readLineSync()!);
-  if (operator == "+") {
-    print("$num1 + $num2 =${num1 + num2}");
-  } else if (operator == "-") {
-    print("$num1 - $num2 = ${num1 - num2}");
-  } else if (operator == "/") {
-    print("$num1 / $num2 = ${num1 / num2}");
-  } else if (operator == "*") {
-    print("$num1 * $num2 = ${num1 * num2}");
-  } else {
-    print("The result not found");
-  }
+  // print('Enter the first operand: ');
+  // num num1 = num.parse(stdin.readLineSync()!);
+  // print('Enter the required operator: ');
+  // String operator = stdin.readLineSync()!;
+  // print('Enter the second operand: ');
+  // num num2 = num.parse(stdin.readLineSync()!);
+  // if (operator == "+") {
+  //   print("$num1 + $num2 =${num1 + num2}");
+  // } else if (operator == "-") {
+  //   print("$num1 - $num2 = ${num1 - num2}");
+  // } else if (operator == "/") {
+  //   print("$num1 / $num2 = ${num1 / num2}");
+  // } else if (operator == "*") {
+  //   print("$num1 * $num2 = ${num1 * num2}");
+  // } else {
+  //   print("The result not found");
+  // }
+  num celsius = 30;
+  num fahrenheit = 9 / 5 * celsius + 32;
+  print('${celsius}C= ${fahrenheit}F');
 }
